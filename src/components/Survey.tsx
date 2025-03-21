a8fae68efb9c2424a6e3709a19249516b7b25a60
Randall Le <randallnguyenle@gmail.com>
2025-03-20 18:01:53 -0700

    added education level

--- src/components/Survey.tsx
+++ src/components/Survey.tsx
@@ -37,12 +37,12 @@ const FormSchema = z.object({
 	age: z.string({
 		required_error: "Please select an age range.",
 	}),
-	// gender: z.string({
-	// 	required_error: "Please select a gender.",
-	// }),
-	// education: z.string({
-	// 	required_error: "Please select an education level.",
-	// }),
+	gender: z.string({
+		required_error: "Please select a gender.",
+	}),
+	education: z.string({
+		required_error: "Please select an education level.",
+	}),
 });
 
 export default function Survey() {
@@ -125,8 +125,152 @@ export default function Survey() {
 									</Command>
 								</PopoverContent>
 							</Popover>
+							<FormDescription></FormDescription>
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name="gender"
+					render={({ field }) => (
+						<FormItem className="flex flex-col">
+							<FormLabel>Gender</FormLabel>
+							<Popover>
+								<PopoverTrigger asChild>
+									<FormControl>
+										<Button
+											variant="outline"
+											role="combobox"
+											className={cn(
+												"w-[200px] justify-between",
+												!field.value &&
+													"text-muted-foreground"
+											)}
+										>
+											{field.value
+												? GENDERS.find(
+														(gender) =>
+															gender.value ===
+															field.value
+												  )?.label
+												: "Select a gender"}
+											<ChevronsUpDown className="opacity-50" />
+										</Button>
+									</FormControl>
+								</PopoverTrigger>
+								<PopoverContent className="w-[200px] p-0">
+									<Command>
+										<CommandInput
+											placeholder="Search genders..."
+											className="h-9"
+										/>
+										<CommandList>
+											<CommandEmpty>
+												No gender found.
+											</CommandEmpty>
+											<CommandGroup>
+												{GENDERS.map((gender) => (
+													<CommandItem
+														value={gender.label}
+														key={gender.value}
+														onSelect={() => {
+															form.setValue(
+																"gender",
+																gender.value
+															);
+														}}
+													>
+														{gender.label}
+														<Check
+															className={cn(
+																"ml-auto",
+																gender.value ===
+																	field.value
+																	? "opacity-100"
+																	: "opacity-0"
+															)}
+														></Check>
+													</CommandItem>
+												))}
+											</CommandGroup>
+										</CommandList>
+									</Command>
+								</PopoverContent>
+							</Popover>
+							<FormDescription></FormDescription>
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name="education"
+					render={({ field }) => (
+						<FormItem className="flex flex-col">
+							<FormLabel>Education</FormLabel>
+							<Popover>
+								<PopoverTrigger asChild>
+									<FormControl>
+										<Button
+											variant="outline"
+											role="combobox"
+											className={cn(
+												"w-[250px] justify-between",
+												!field.value &&
+													"text-muted-foreground"
+											)}
+										>
+											{field.value
+												? EDUCATION.find(
+														(education) =>
+															education.value ===
+															field.value
+												  )?.label
+												: "Select an education level"}
+											<ChevronsUpDown className="opacity-50" />
+										</Button>
+									</FormControl>
+								</PopoverTrigger>
+								<PopoverContent className="w-[250px] p-0">
+									<Command>
+										<CommandInput
+											placeholder="Search education levels..."
+											className="h-9"
+										/>
+										<CommandList>
+											<CommandEmpty>
+												No education level found.
+											</CommandEmpty>
+											<CommandGroup>
+												{EDUCATION.map((education) => (
+													<CommandItem
+														value={education.label}
+														key={education.value}
+														onSelect={() => {
+															form.setValue(
+																"education",
+																education.value
+															);
+														}}
+													>
+														{education.label}
+														<Check
+															className={cn(
+																"ml-auto",
+																education.value ===
+																	field.value
+																	? "opacity-100"
+																	: "opacity-0"
+															)}
+														></Check>
+													</CommandItem>
+												))}
+											</CommandGroup>
+										</CommandList>
+									</Command>
+								</PopoverContent>
+							</Popover>
 							<FormDescription>
-								Please select the age group you along to.
+								Please select your education level.
 							</FormDescription>
 						</FormItem>
 					)}
