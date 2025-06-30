573e523cd2438e1cd5e707c5186ec5b525c06e4f
Randall Le <randallnguyenle@gmail.com>
2025-06-29 17:24:50 -0700

    started game component

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,12 +1,42 @@
 "use client";
 
-import Board from "@/components/Board";
-import { getRandomProblemOrder } from "@/lib/utils";
+// import Board from "@/components/Board";
+// import { getRandomProblemOrder } from "@/lib/utils";
+import { useState } from "react";
+import PreGameModal from "@/components/PreGameModal";
+import CountdownTimer from "@/components/CountdownTimer";
+// import { TRIAL_NAMES } from "@/lib/constants";
 
 export default function Game() {
-	// modal
-	// timer
+	const [trialIndex, setTrialIndex] = useState(0);
+	const [startMusic, setStartMusic] = useState(false);
+	const [startGame, setStartGame] = useState(false);
+	const [trialComplete, setTrialComplete] = useState(false);
+
+	// timer for test
 	// board: pops problem from randomProblems
 	// submit button: save to sessionStorage
-	return <div></div>;
+	return (
+		<div>
+			<PreGameModal
+				trialIndex={trialIndex}
+				setTrialIndex={setTrialIndex}
+				startMusic={startMusic}
+				setStartMusic={setStartMusic}
+				startGame={startGame}
+				setStartGame={setStartGame}
+				trialComplete={trialComplete}
+				setTrialComplete={setTrialComplete}
+			/>
+			<CountdownTimer
+				initialTime={60}
+				onComplete={() => {
+					if (!trialComplete) {
+						setTrialComplete(true);
+					}
+				}}
+				startCondition={startGame}
+			></CountdownTimer>
+		</div>
+	);
 }
