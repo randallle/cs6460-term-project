248a67b4897f1af55980c86b160c87809b408af2
Randall Le <randallnguyenle@gmail.com>
2025-06-29 19:03:16 -0700

    added StartTrialModal to game comp

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -3,7 +3,11 @@
 // import Board from "@/components/Board";
 // import { getRandomProblemOrder } from "@/lib/utils";
 import { useState } from "react";
+import StartTrialModal from "@/components/StartTrialModal";
 import PreGameModal from "@/components/PreGameModal";
+import EndTrialModal from "@/components/EndTrialModal";
+import EndTestModal from "@/components/EndTestModal";
+
 import CountdownTimer from "@/components/CountdownTimer";
 // import { TRIAL_NAMES } from "@/lib/constants";
 
@@ -16,5 +20,20 @@ export default function Game() {
 	// timer for test
 	// board: pops problem from randomProblems
 	// submit button: save to sessionStorage
-	return <div></div>;
+	return (
+		<div>
+			{!startMusic && !startGame && (
+				<StartTrialModal
+					trialIndex={trialIndex}
+					startMusic={startMusic}
+					setStartMusic={setStartMusic}
+				/>
+			)}
+
+			{/* {startMusic && !startGame && (
+				<EndTestModal
+                />
+			)} */}
+		</div>
+	);
 }
