6445b6cf8925da4c2e1d5473d9bfb78411915ba1
Randall Le <randallnguyenle@gmail.com>
2025-06-29 17:57:25 -0700

    skeleton of starttrialmodal

---
+++ src/components/StartTrialModal.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import {
+	Dialog,
+	DialogContent,
+	DialogHeader,
+	DialogTitle,
+	DialogDescription,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { TRIAL_NAMES } from "@/lib/constants";
+
+interface StartTrialModalProps {
+	trialIndex: number;
+	startMusic: boolean;
+	setStartMusic: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function StartTrialModal({
+	trialIndex,
+	startMusic,
+	setStartMusic,
+}: StartTrialModalProps) {
+	return (
+		<Dialog>
+			<DialogContent className="max-w-md">
+				<DialogHeader>
+					<DialogTitle className="text-xl font-semibold text-center"></DialogTitle>
+					<DialogDescription></DialogDescription>
+				</DialogHeader>
+				<div className="flex flex-col items-center gap-6 mt-4"></div>
+			</DialogContent>
+		</Dialog>
+	);
+}
