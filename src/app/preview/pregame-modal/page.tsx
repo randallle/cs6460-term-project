fadd229931fb151c1806a4185c01351cf0c84396
Randall Le <randallnguyenle@gmail.com>
2025-06-29 18:12:23 -0700

    changed logic for next trial button

--- src/app/preview/pregame-modal/page.tsx
+++ src/app/preview/pregame-modal/page.tsx
@@ -3,18 +3,30 @@
 import { useState } from "react";
 import PreGameModal from "@/components/PreGameModal";
 import StartTrialModal from "@/components/StartTrialModal";
+import EndTrialModal from "@/components/EndTrialModal";
 
 export default function PreGameModalPreview() {
 	const [startMusic, setStartMusic] = useState(true);
 	const [startGame, setStartGame] = useState(true);
 	const [trialIndex, setTrialIndex] = useState(0);
 	const [trialComplete, setTrialComplete] = useState(true);
+	const [testComplete, setTestComplete] = useState(false);
 
 	return (
-		<StartTrialModal
+		// <StartTrialModal
+		// 	trialIndex={trialIndex}
+		// 	startMusic={startMusic}
+		// 	setStartMusic={setStartMusic}
+		// />
+
+		<EndTrialModal
 			trialIndex={trialIndex}
 			startMusic={startMusic}
+			setTrialIndex={setTrialIndex}
+			setTestComplete={setTestComplete}
+			setStartGame={setStartGame}
 			setStartMusic={setStartMusic}
+			setTrialComplete={setTrialComplete}
 		/>
 	);
 }
