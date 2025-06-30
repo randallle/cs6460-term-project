d479244414643070fd4ed19631c263442dafada2
Randall Le <randallnguyenle@gmail.com>
2025-06-29 19:22:42 -0700

    added fetch problem by ID function

--- src/lib/firebaseUtils.ts
+++ src/lib/firebaseUtils.ts
@@ -44,6 +44,38 @@ export async function fetchRandomProblemByDifficulty(difficulty: string) {
 	};
 }
 
+export async function fetchProblemById(id: string) {
+	const problemsRef = collection(db, "problems");
+
+	// Query Firestore for the specific problem by ID
+	const q = query(problemsRef, where("id", "==", id));
+	const querySnapshot = await getDocs(q);
+
+	if (querySnapshot.empty) {
+		throw new Error(`No problem found with ID ${id}`);
+	}
+
+	// Since IDs are unique, we expect only one document
+	const problem = querySnapshot.docs[0];
+	const problemData = problem.data();
+
+	// Fetch associated images from Firebase Storage
+	const matrixImages = await fetchImagesFromStorage(
+		`problems/${problemData.id}/matrix`
+	);
+
+	const choiceImages = await fetchImagesFromStorage(
+		`problems/${problemData.id}/choices`
+	);
+
+	// Return the problem data along with the images
+	return {
+		...(problemData as ProblemData),
+		matrix: matrixImages,
+		choices: choiceImages,
+	};
+}
+
 async function fetchImagesFromStorage(folderPath: string) {
 	const folderRef = ref(storage, folderPath);
 	const fileList = await listAll(folderRef);
