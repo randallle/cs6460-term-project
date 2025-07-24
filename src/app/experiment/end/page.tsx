4729f892208f69fefd8c1b96e29490081bdf122d
Randall Le <randallnguyenle@gmail.com>
2025-07-23 18:34:02 -0700

    removed background color

--- src/app/experiment/end/page.tsx
+++ src/app/experiment/end/page.tsx
@@ -1,6 +1,6 @@
 export default function ExperimentPage() {
 	return (
-		<div className="min-h-screen flex items-center justify-center bg-gray-50">
+		<div className="min-h-screen flex items-center justify-center">
 			<div className="text-center">
 				<h1 className="text-4xl font-bold text-gray-800 mb-4">
 					Thank you for participating.
